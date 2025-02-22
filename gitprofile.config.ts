a89323ed7d06bbb427001446ca09980d172d10e4
Raviraj Sonwalkar <raviwork2802@gmail.com>
2025-02-21 19:41:05 -0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,245 +2,126 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'ravirajsonwalkar', // Replace with your GitHub username
   },
-  /**
-   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
-   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
-   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
-   */
-  base: '/gitprofile/',
-  projects: {
-    github: {
-      display: true, // Display GitHub projects?
-      header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
-      automatic: {
-        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
-        exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
-        },
-      },
-      manual: {
-        // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
-      },
-    },
-    external: {
-      header: 'My Projects',
-      // To hide the `External Projects` section, keep it empty.
-      projects: [
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-      ],
-    },
-  },
-  seo: {
-    title: 'Portfolio of Ariful Alam',
-    description: '',
-    imageURL: '',
-  },
+  base: '/',
   social: {
-    linkedin: 'ariful-alam',
-    x: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    researchGate: '',
-    facebook: '',
-    instagram: '',
-    reddit: '',
-    threads: '',
-    youtube: '', // example: 'pewdiepie'
-    udemy: '',
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
-    telegram: '',
-    website: 'https://www.arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    linkedin: 'raviraj-sonwalkar',
+    email: 'raviwork2802@gmail.com',
   },
   resume: {
-    fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+    fileUrl: 'https://www.dropbox.com/scl/fi/bggmffz9yguhql3s5w0bw/Raviraj_S_Resume-1.pdf?rlkey=evsu1oj3gliyvsz90lmnlia49&st=qfvzch1x&dl=1', // Add your resume URL here
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'AI Frameworks',
+    'AI in Software Development',
+    'AI Agents',
+    'Python',
+    'NumPy',
+    'Pandas',
+    'Scikit-Learn',
+    'Data Analysis',
+    'Predictive Modeling',
+    'Business Analytics'
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Oakland Natives Give Back',
+      position: 'Data Scientist',
+      from: 'July 2024',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://ongb.org/',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'East Sunshine Ventures',
+      position: 'Venture Capital Intern',
+      from: 'Mar 2024',
+      to: 'June 2024',
+      companyLink: 'https://www.eastsunshineventures.com/',
     },
+    {
+      company: 'FPL Technologies',
+      position: 'Data Analyst, Customer Success',
+      from: 'July 2021',
+      to: 'April 2022',
+      companyLink: 'https://www.fplabs.tech/',
+    },
+    {
+      company: 'Amazon',
+      position: 'Business Analyst, Customer Success',
+      from: 'August 2020',
+      to: 'March 2021',
+      companyLink: 'https://www.aboutamazon.in/',
+    }
   ],
   certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
+      name: 'Multi AI Agent Systems with crewAI',
+      body: '',
+      year: '2024',
+      link: 'https://learn.deeplearning.ai/accomplishments/5b553fe9-5c54-47c0-bf42-d66a160ad83a?usp=sharing',
     },
+    {
+      name: 'Building Generative AI Applications with Gradio',
+      body: '',
+      year: '2024',
+      link: 'https://learn.deeplearning.ai/accomplishments/28b02d7d-ba94-40b9-838a-7d409bd71532?usp=sharing',
+    },
+    {
+      name: 'Build Long-Context AI Apps with Jamba',
+      body: '',
+      year: '2024',
+      link: 'https://learn.deeplearning.ai/accomplishments/32f53d07-c245-4be8-aad4-6da3378655d6?usp=sharing',
+    },
+    {
+      name: 'AI Agents Fundamentals',
+      body: '',
+      year: '2024',
+      link: 'https://cdn-lfs-us-1.hf.co/repos/f2/34/f2344151f60f6027c436821dc61cf3f27a46435de57df8df50ad02b5acca7c07/8a26afc4294cdfff0f1ea762212d93f0b3fd74bdc8b93d2bb47c74bb6ea8fc93?response-content-disposition=inline%3B+filename*%3DUTF-8%27%272025-02-11.png%3B+filename%3D%222025-02-11.png%22%3B&response-content-type=image%2Fpng&Expires=1740199158&Policy=eyJTdGF0ZW1lbnQiOlt7IkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTc0MDE5OTE1OH19LCJSZXNvdXJjZSI6Imh0dHBzOi8vY2RuLWxmcy11cy0xLmhmLmNvL3JlcG9zL2YyLzM0L2YyMzQ0MTUxZjYwZjYwMjdjNDM2ODIxZGM2MWNmM2YyN2E0NjQzNWRlNTdkZjhkZjUwYWQwMmI1YWNjYTdjMDcvOGEyNmFmYzQyOTRjZGZmZjBmMWVhNzYyMjEyZDkzZjBiM2ZkNzRiZGM4YjkzZDJiYjQ3Yzc0YmI2ZWE4ZmM5Mz9yZXNwb25zZS1jb250ZW50LWRpc3Bvc2l0aW9uPSomcmVzcG9uc2UtY29udGVudC10eXBlPSoifV19&Signature=oYskHVGrWv6cpiuo6XacLOv6vlwb0FVY1WYvmTIHYA1gFpO2XHeXsuMSaYVTwXwGdJhDAmMu3dSkLOP0qmKILTt4c-sR0dIOkk3tlLaqwbOfoeyynYBZkNUPfkSMZ8AW%7E7KxTxyHR9R-X9OmbHLficnxgCb3OALX1uWgVqwOs7zJNwVKeKwvxLDiMZudo3s4YLkNgh9ZSyFXNqVCmybIY2ariPxAow6b9CQJOTbVv%7EbxJAwLSNcaoknVv%7Ene-Utam-YheeYl85j59vdBkDG7tZMLTMj-60Bd90uQ7jRa8h9Zq4IYgxHzMk3Tf78QpXcvJ%7Eb1MCGig%7ETDQx342P86tg__&Key-Pair-Id=K24J24Z295AEI9',
+    }
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Hult International Business School',
+      degree: 'Master of Science - MS, Business Analytics',
+      from: '2023',
+      to: '2024',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Hult International Business School',
+      degree: 'Master of Science - MS, International Business',
+      from: '2022',
+      to: '2023',
     },
+    {
+      institution: 'Haroun Education Ventures MBA Degree Program',
+      degree: 'Master of Business Administration - MBA',
+      from: '2020',
+      to: '2021',
+    },
+    {
+      institution: 'MIT ADT University',
+      degree: "Bachelor's degree, Civil Engineering",
+      from: '2017',
+      to: '2020',
+    }
   ],
-  publications: [
-    {
-      title: 'Publication Title',
-      conferenceName: '',
-      journalName: 'Journal Name',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      title: 'Publication Title',
-      conferenceName: 'Conference Name',
-      journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-  ],
-  // Display articles from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
-  },
-  googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
-  // Track visitor interaction and behavior. https://www.hotjar.com
-  hotjar: {
-    id: '',
-    snippetVersion: 6,
+  seo: {
+    title: 'Portfolio of Raviraj Sonwalkar',
+    description: 'Data Scientist and VC professional with expertise in AI Engineering and Business analytics',
   },
   themeConfig: {
-    defaultTheme: 'lofi',
-
-    // Hides the switch in the navbar
-    // Useful if you want to support a single color mode
+    defaultTheme: 'corporate',
     disableSwitch: false,
-
-    // Should use the prefers-color-scheme media-query,
-    // using user system preferences, instead of the hardcoded defaultTheme
-    respectPrefersColorScheme: false,
-
-    // Display the ring in Profile picture
-    displayAvatarRing: true,
-
-    // Available themes. To remove any theme, exclude from here.
+    respectPrefersColorScheme: true,
     themes: [
       'light',
       'dark',
-      'cupcake',
-      'bumblebee',
-      'emerald',
       'corporate',
-      'synthwave',
-      'retro',
-      'cyberpunk',
-      'valentine',
-      'halloween',
-      'garden',
-      'forest',
-      'aqua',
-      'lofi',
-      'pastel',
-      'fantasy',
-      'wireframe',
-      'black',
       'luxury',
-      'dracula',
-      'cmyk',
-      'autumn',
       'business',
-      'acid',
-      'lemonade',
-      'night',
-      'coffee',
-      'winter',
-      'dim',
-      'nord',
-      'sunset',
-      'procyon',
     ],
-
-    // Custom theme, applied to `procyon` theme
-    customTheme: {
-      primary: '#fc055b',
-      secondary: '#219aaf',
-      accent: '#e8d03a',
-      neutral: '#2A2730',
-      'base-100': '#E3E3ED',
-      '--rounded-box': '3rem',
-      '--rounded-btn': '3rem',
-    },
   },
-
-  // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
-
-  enablePWA: true,
 };
 
 export default CONFIG;
