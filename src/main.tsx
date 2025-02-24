49cf696c89b056a4c818059715b62adce464db8e
Raviraj Sonwalkar <raviwork2802@gmail.com>
2025-02-23 17:45:37 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GitProfile from './components/gitprofile.tsx';
+import GitProfile from '@arifszn/gitprofile';
+import '@arifszn/gitprofile/dist/style.css';
+import CONFIG from '../gitprofile.config';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
