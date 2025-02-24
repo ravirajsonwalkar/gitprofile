5a7269d41011068309ceca12c4530495f43d500c
Raviraj Sonwalkar <raviwork2802@gmail.com>
2025-02-23 17:46:13 -0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import GitProfile from '@arifszn/gitprofile';
+import '@arifszn/gitprofile/dist/style.css';
+import CONFIG from '../gitprofile.config';
+
+function App() {
+  return <GitProfile config={CONFIG} />;
+}
+
+export default App;
